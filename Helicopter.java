4484520131388413417c9fc4cda0c11df3d94608
Kaila <156868409+quimsonchin@users.noreply.github.com>
2024-10-16 14:29:57 -0400

    Update Helicopter.java

--- Helicopter.java
+++ Helicopter.java
@@ -1,5 +1,43 @@
-public class Helicopter extends Airplane{
-    int numberOfCylinders;
-    int creationYear;
-    int passangerCapacity;
+
+public class Helicopter extends Airplane {
+	
+		//Helicopter attributes
+		private int numberOfCylinders;
+	    private int creationYear;
+	    private int passengerCapacity;
+
+	    
+	    //parameterized constructor
+	    public Helicopter(String brand, double price,int horsePower, int numberOfCylinders, int creationYear, int passengerCapacity){
+	    	super(brand, price, horsePower);
+	        this.numberOfCylinders = numberOfCylinders;
+	        this.creationYear = creationYear;
+	        this.passengerCapacity = passengerCapacity;
+
+	    }
+	    
+	    //setters
+	    public int getNumberOfCylinders() {
+	    	return this.numberOfCylinders;
+	    }
+	    
+	    public int getCreationYear() {
+	    	return this.creationYear;
+	    }
+	    
+	    public int getPassengerCapacity() {
+	    	return this.passengerCapacity;
+	    }
+	    
+	    public int setNumberOfCylinders() {
+	    	return this.numberOfCylinders;
+	    }
+	    
+	    public int setCreationYear() {
+	    	return this.creationYear;
+	    }
+	    
+	    public int setPassengerCapacity() {
+	    	return this.passengerCapacity;
+	    }
 }
