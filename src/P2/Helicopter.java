09681d945a5d5840d6d6c447748f6de4a5ee28a3
Kaila <156868409+quimsonchin@users.noreply.github.com>
2024-10-16 15:18:04 -0400

    copy constructor Helicopter.java

--- src/P2/Helicopter.java
+++ src/P2/Helicopter.java
@@ -7,45 +7,49 @@ public class Helicopter extends Airplane {
 	private int numberOfCylinders;
 	private int creationYear;
 	private int passengerCapacity;
-
+	
 	public Helicopter() {
-	    this.numberOfCylinders = 0;
-	    this.creationYear = 0;
-	    this.passengerCapacity = 0;
-	    }
-
-	    
-	    //parameterized constructor
-	    public Helicopter(String brand, double price, int horsePower, int numberOfCylinders, int creationYear, int passengerCapacity){
-	    	super(brand, price, horsePower);
-	        this.numberOfCylinders = numberOfCylinders;
-	        this.creationYear = creationYear;
-	        this.passengerCapacity = passengerCapacity;
-
-	    }
-	    
-	    //setters
-	    public int getNumberOfCylinders() {
-	    	return this.numberOfCylinders;
-	    }
-	    
-	    public int getCreationYear() {
-	    	return this.creationYear;
-	    }
-	    
-	    public int getPassengerCapacity() {
-	    	return this.passengerCapacity;
-	    }
-	    
-	    public int setNumberOfCylinders() {
-	    	return this.numberOfCylinders;
-	    }
-	    
-	    public int setCreationYear() {
-	    	return this.creationYear;
-	    }
-	    
-	    public int setPassengerCapacity() {
-	    	return this.passengerCapacity;
-	    }
+	this.numberOfCylinders = 0;
+	this.creationYear = 0;
+	this.passengerCapacity = 0;
+	}
+	
+	
+	//parameterized constructor
+	public Helicopter(String brand, double price, int horsePower, int numberOfCylinders, int creationYear, int passengerCapacity){
+	super(brand, price, horsePower);
+	this.numberOfCylinders = numberOfCylinders;
+	this.creationYear = creationYear;
+	this.passengerCapacity = passengerCapacity;
+	
+	}
+	//copy constructor
+	public Helicopter(Helicopter copy) {
+	this(copy.brand, copy.price, copy.horsePower, copy.numberOfCylinders, copy.creationYear, copy.passengerCapacity);
+	}
+	
+	//setters
+	public int getNumberOfCylinders() {
+	return this.numberOfCylinders;
+	}
+	
+	public int getCreationYear() {
+	return this.creationYear;
+	}
+	
+	public int getPassengerCapacity() {
+	return this.passengerCapacity;
+	}
+	
+	public int setNumberOfCylinders() {
+	return this.numberOfCylinders;
+	}
+	
+	public int setCreationYear() {
+	return this.creationYear;
+	}
+	
+	public int setPassengerCapacity() {
+	return this.passengerCapacity;
+	}
 }
