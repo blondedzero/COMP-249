c935f1f25f206dab986ebc7c949de85ff150ba99
Kaila <156868409+quimsonchin@users.noreply.github.com>
2024-10-16 14:34:14 -0400

    Update Quadcopter.java

--- Quadcopter.java
+++ Quadcopter.java
@@ -1,3 +1,21 @@
-public class Quadcopter{
+public class Quadcopter extends Helicopter{
+	
+	private int maxFlyingSpeed;
+	
+    public Quadcopter(String brand, double price, int horsePower, int numberOfCylinders, int creationYear,
+			int passengerCapacity, int maxFlyingSpeed) {
+		super(brand, price, horsePower, numberOfCylinders, creationYear, passengerCapacity);
+		
+		this.maxFlyingSpeed = maxFlyingSpeed;
+    }
     
+    public int getMaxFlyingSpeed() {
+    	return this.maxFlyingSpeed;
+    }
+    
+    public int gsetMaxFlyingSpeed() {
+    	return this.maxFlyingSpeed;
+    }
+
+	
 }
