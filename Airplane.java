289ce049714f4b6c07cf9c6c8dc12d7437796a76
Kaila <156868409+quimsonchin@users.noreply.github.com>
2024-10-16 14:37:36 -0400

    added initializer

--- Airplane.java
+++ Airplane.java
@@ -1,34 +1,40 @@
 
 public class Airplane {
 	
-		protected String brand;
-		protected double price;
-	    protected int horsePower;
+	protected String brand;
+	protected double price;
+	protected int horsePower;
+	
+	public Airplane() {
+		this.brand = "";
+		this.price = 0.0;
+		this.horsePower = 0;
+	}
 
-	    public Airplane(String brand, double price, int horsePower){
-	        this.brand = brand;
-	        this.price = price;
-	        this.horsePower = horsePower;
-	    }
+    public Airplane(String brand, double price, int horsePower){
+	this.brand = brand;
+	this.price = price;
+	this.horsePower = horsePower;
+    }
 
-	    public String getBrand(){
-	        return this.brand;
-	    }
-	    public double getPrice(){
-	        return this.price;
-	    }
-	    public int getHorsePower(){
-	        return this.horsePower;
-	    }
+    public String getBrand(){
+	return this.brand;
+    }
+    public double getPrice(){
+	return this.price;
+    }
+    public int getHorsePower(){
+	return this.horsePower;
+    }
 
-	    public String setBrand(){
-	        return this.brand;
-	    }
-	    public double setPrice(){
-	        return this.price;
-	    }
-	    public int setHorsePower(){
-	        return this.horsePower;
-	    }
+    public String setBrand(){
+	return this.brand;
+    }
+    public double setPrice(){
+	return this.price;
+    }
+    public int setHorsePower(){
+	return this.horsePower;
+    }
 
 }
