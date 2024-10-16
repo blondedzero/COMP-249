a0ac959f70cf96e006311b0b8e23a97202a39504
Kaila <156868409+quimsonchin@users.noreply.github.com>
2024-10-16 15:06:18 -0400

    default constructor quadcopter.java

--- src/P2/Quadcopter.java
+++ src/P2/Quadcopter.java
@@ -1,22 +1,26 @@
 package P2;
 public class Quadcopter extends Helicopter{
-	
-	private int maxFlyingSpeed;
-	
-    public Quadcopter(String brand, double price, int horsePower, int numberOfCylinders, int creationYear,
-			int passengerCapacity, int maxFlyingSpeed) {
-		super(brand, price, horsePower, numberOfCylinders, creationYear, passengerCapacity);
-		
-		this.maxFlyingSpeed = maxFlyingSpeed;
-    }
-    
-    public int getMaxFlyingSpeed() {
-    	return this.maxFlyingSpeed;
-    }
-    
-    public int gsetMaxFlyingSpeed() {
-    	return this.maxFlyingSpeed;
-    }
 
+private int maxFlyingSpeed;
+
+public Quadcopter() {
+	this.maxFlyingSpeed = 0;
+}
+
+public Quadcopter(String brand, double price, int horsePower, int numberOfCylinders, int creationYear,
+		int passengerCapacity, int maxFlyingSpeed) {
+	super(brand, price, horsePower, numberOfCylinders, creationYear, passengerCapacity);
 	
+	this.maxFlyingSpeed = maxFlyingSpeed;
+}
+
+public int getMaxFlyingSpeed() {
+return this.maxFlyingSpeed;
+}
+
+public int gsetMaxFlyingSpeed() {
+return this.maxFlyingSpeed;
+}
+
+
 }
