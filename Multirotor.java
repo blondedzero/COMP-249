5058b6c1100a17f5f2dadc4384c5b6ad512f5c61
Kaila <156868409+quimsonchin@users.noreply.github.com>
2024-10-16 14:30:52 -0400

    Update Multirotor.java

--- Multirotor.java
+++ Multirotor.java
@@ -1,5 +1,21 @@
 public class Multirotor extends Helicopter{
-
-    int numberOfRotors;
-    
+	
+	private int numberOfRotors;
+	
+	public Multirotor(String brand, double price, int horsePower, int numberOfCylinders, int creationYear,
+			int passengerCapacity, int numberOfRotors) {
+		super(brand, price, horsePower, numberOfCylinders, creationYear, passengerCapacity);
+		
+		this.numberOfRotors = numberOfRotors;
+	}
+	
+		//getter
+	   public int getNumberOfRotors() {
+	    	return this.numberOfRotors;
+	    }
+		
+	   //setter
+	   public int setNumberOfRotors() {
+	    	return this.numberOfRotors;
+	    }    
 }
