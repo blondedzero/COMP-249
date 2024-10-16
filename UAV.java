4f3d0ed1aac8417e1c9ea1c653b44a8273afc38b
Kaila <156868409+quimsonchin@users.noreply.github.com>
2024-10-16 14:41:23 -0400

    Update UAV.java

--- UAV.java
+++ UAV.java
@@ -1,4 +1,31 @@
 public class UAV {
-   double weight;
-   double price;
+   private double weight;
+   private double price;
+   
+   public UAV() {
+	   this.weight = 0.0;
+	   this.price = 0.0;
+   }
+   
+   public UAV(double weight, double price) {
+	   
+	   this.weight = weight;
+	   this.price = price;
+   }
+   
+   public double getUAVWeight() {
+	   return this.weight;
+   }
+   
+   public double getUAVPrice() {
+	   return this.price;
+   }
+   
+   public double setUAVWeight() {
+	   return this.weight;
+   }
+   
+   public double setUAVPrice() {
+	   return this.price;
+   }
 }
