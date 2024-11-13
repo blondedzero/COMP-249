50e50029abf5070e6115dd3cdc287e3e62c5f953
quimsonchin <k_quimso@live.concordia.ca>
2024-11-13 17:14:19 -0500

    prints to terminal but not to files

--- BookCatalog.java
+++ BookCatalog.java
@@ -12,7 +12,7 @@ import java.util.*;
 
 public class BookCatalog {
 
-	private static PrintWriter errorWriter;
+		private static PrintWriter errorWriter;
 
 		// Arrays for genre codes and corresponding output file names
 		private static final String[] genreCodes = {"CCB", "HCB", "MTV", "MRB", "NEB", "OTR", "SSM", "TPA"};
@@ -27,15 +27,13 @@ public class BookCatalog {
 			"Trains_Planes_Automobiles.csv"
 		};
 
-		public static void main(String[] args) throws TooFewFieldsException, TooManyFieldsException, MissingFieldException, UnknownGenreException {
+		public static void main(String[] args) throws TooFewFieldsException, TooManyFieldsException, MissingFieldException, UnknownGenreException, IOException {
 		{
 			try {
-				errorWriter = new PrintWriter(new FileWriter("syntax_error_file.txt"));
+				//errorWriter = new PrintWriter(new FileWriter("syntax_error_file.txt"));
 				do_part1();
 				do_part2();
 				do_part3();
-			} catch (IOException e) {
-				System.out.println("File initializing error: " + e.getMessage());
 			} finally{
 				if (errorWriter != null){
 					errorWriter.close();
@@ -50,10 +48,12 @@ public class BookCatalog {
 		public static void do_part1() throws TooFewFieldsException, TooManyFieldsException, MissingFieldException, UnknownGenreException{
 
 		try {
+			
 			FileReader myFileReader = new FileReader("txtFiles/Part1_input_file_names.txt");
 			BufferedReader br = new BufferedReader(myFileReader);
 
 			int numberOfFiles = Integer.parseInt(br.readLine());
+			System.out.println("Number of Files: " + numberOfFiles);
 
 			String filename;
 			//READS LINES UNTIL IT REACHES AN EMPTY LINE WHICH WILL CASUE THE LOOP TO STOP
@@ -86,7 +86,7 @@ public class BookCatalog {
 
 			BufferedWriter[] writers = new BufferedWriter[genreCodes.length];
 
-			
+			System.out.println("Inside Process file: " + filename);
 				
 			try {
 				FileReader myFileReader = new FileReader(filename);
@@ -104,6 +104,8 @@ public class BookCatalog {
 					//THE CHECKFIELDS IS CALLES AND STORES IN A NEW ARRAY
 					String[] allFields = checkFields(line);
 					
+					System.out.println("All fields: " + Arrays.toString(allFields));
+
 					//CHECKS IF THERE ARE NOT ENOUGH FIELDS AND TROWS AN EXCEPTION
 					if (allFields.length < 6) {
 						throw new TooFewFieldsException("Too Few Fields!");
@@ -114,6 +116,7 @@ public class BookCatalog {
 						throw new TooManyFieldsException("Too Many Fields!"); 
 					}
 					// FIELDS HAVE BEEN VERIFIED. 
+					System.out.println("Fields have been verified");
 
 					//CALLS THE MISSING FIELDS FUNCITON AND STORES RETURN INTO A VARIABLE 
 					String missingField = checkMissingField(allFields);
@@ -123,23 +126,34 @@ public class BookCatalog {
 						throw new MissingFieldException(missingField);
 					}
 
+
 					//CHECKS FOR ISBN VALIDITY
 					String isbn = allFields[3]; //ISBN IS FOUND IN POSITION 3 OF THE ARRAY STORING IT IN THE ISBN VARIABLE
-					if(checkISBN10(isbn) == true || checkISBN13(isbn) == true) {
-						System.out.println("VALILD ISBN");
+					if(isbn.length() == 10){
+						if(checkISBN10(isbn) == true) {
+							System.out.println("VALILD ISBN");
+						} else{
+							logSyntaxError(filename, "Invalid ISBN", line);
+						}
+					}else{
+						if(checkISBN13(isbn) == true) {
+							System.out.println("VALILD ISBN");
+						}else{
+							logSyntaxError(filename, "Invalid ISBN", line);
+						}
 					}
 
-					if (checkISBN10(isbn) == false || checkISBN13(isbn) == false){
-						System.out.println("Error: invalid ISBN");
-						continue;
-					}
+					System.out.println("ISBN verified.");
 
 					//CHECKS FOR GENRE VALIDITY
 					String genreCode = allFields[4];
                 	int genreIndex = checkGenre(genreCode);
+
+					System.out.println(genreIndex);
+
                 	if (genreIndex == -1) {
-                    logSyntaxError(filename, "Unknown Genre: " + genreCode, line);
-                    continue;
+						logSyntaxError(filename, "Unknown Genre: " + genreCode, line);
+						throw new UnknownGenreException(genreCode);
                		}
 
 					//CHECKS FOR YEAR VALIDITY
@@ -150,12 +164,25 @@ public class BookCatalog {
 					}
 
 					//WRITE TO SPECIFIC GENRE FILE
+					System.out.println("Line 160"+line);
 					writers[genreIndex].write(line);
 					writers[genreIndex].newLine();
 				}
-				} catch (IOException e) {
-					System.out.println("Error reading file: " + filename);
-				} finally {
+				} catch (TooFewFieldsException e) {
+					System.out.println("Too few fields");
+				} catch (UnknownGenreException ug){
+					System.out.println("unknown genre ");
+				} catch (TooManyFieldsException tmf){System.out.println("too many fields");
+
+				}catch (MissingFieldException mf){
+					System.out.println("missing fields " );
+					
+				}
+				catch (IOException e){
+					System.out.println("file not found");
+				}
+
+				finally {
 					//CLOSE GENRE WRITERS
 					for (BufferedWriter writer : writers) {
 						if (writer != null) {
@@ -214,27 +241,7 @@ public class BookCatalog {
 
 
   			private static boolean checkISBN10(String ISBN){
-           		//"1234565219"
-              int x1 = Integer.parseInt(ISBN.substring(0,1));
-              int x2 = Integer.parseInt(ISBN.substring(1,2));
-              int x3 = Integer.parseInt(ISBN.substring(2,3));
-              int x4 = Integer.parseInt(ISBN.substring(3,4));
-              int x5 = Integer.parseInt(ISBN.substring(4,5));
-              int x6 = Integer.parseInt(ISBN.substring(5,6));
-              int x7 = Integer.parseInt(ISBN.substring(6,7));
-              int x8 = Integer.parseInt(ISBN.substring(7,8));
-              int x9 = Integer.parseInt(ISBN.substring(8,9));
-              int x10 = Integer.parseInt(ISBN.substring(9,10));
-              
-              int sum = (10 * x1) + (9 * x2) + (8*x3) + (7*x4) + (6*x5) + (5*x6) + (4*x7) + (3*x8) + (2*x9)+ (1*x10);
-              
-              if(sum % 11 == 0){
-              	return true;
-              } else { return false; }
-            }
-
-			private static boolean checkISBN13(String ISBN){
-				
+           	try{
 				int x1 = Integer.parseInt(ISBN.substring(0,1));
 				int x2 = Integer.parseInt(ISBN.substring(1,2));
 				int x3 = Integer.parseInt(ISBN.substring(2,3));
@@ -245,15 +252,41 @@ public class BookCatalog {
 				int x8 = Integer.parseInt(ISBN.substring(7,8));
 				int x9 = Integer.parseInt(ISBN.substring(8,9));
 				int x10 = Integer.parseInt(ISBN.substring(9,10));
-				int x11 = Integer.parseInt(ISBN.substring(9,11));
-				int x12 = Integer.parseInt(ISBN.substring(9,12));
-				int x13 = Integer.parseInt(ISBN.substring(9,13));
 				
-				int sum = x1 + (3 * x2) + x3 + (3*x4) + x5 + (3*x6) + x7 + (3*x8) + x9 + (3*x10)+ x11 + (3*x12)+ x11 + (3*x13);
+				int sum = (10 * x1) + (9 * x2) + (8*x3) + (7*x4) + (6*x5) + (5*x6) + (4*x7) + (3*x8) + (2*x9)+ (1*x10);
 				
-				if(sum % 13 == 0){
+				if(sum % 11 == 0){
 					return true;
 				} else { return false; }
+				}catch(NumberFormatException nfe){
+					return false;
+				}
+            }
+
+			private static boolean checkISBN13(String ISBN){
+				try {
+					int x1 = Integer.parseInt(ISBN.substring(0,1));
+					int x2 = Integer.parseInt(ISBN.substring(1,2));
+					int x3 = Integer.parseInt(ISBN.substring(2,3));
+					int x4 = Integer.parseInt(ISBN.substring(3,4));
+					int x5 = Integer.parseInt(ISBN.substring(4,5));
+					int x6 = Integer.parseInt(ISBN.substring(5,6));
+					int x7 = Integer.parseInt(ISBN.substring(6,7));
+					int x8 = Integer.parseInt(ISBN.substring(7,8));
+					int x9 = Integer.parseInt(ISBN.substring(8,9));
+					int x10 = Integer.parseInt(ISBN.substring(9,10));
+					int x11 = Integer.parseInt(ISBN.substring(9,11));
+					int x12 = Integer.parseInt(ISBN.substring(9,12));
+					int x13 = Integer.parseInt(ISBN.substring(9,13));
+					
+					int sum = x1 + (3 * x2) + x3 + (3*x4) + x5 + (3*x6) + x7 + (3*x8) + x9 + (3*x10)+ x11 + (3*x12)+ x11 + (3*x13);
+				
+					if(sum % 13 == 0){
+						return true;
+					} else { return false; }
+				}catch(NumberFormatException nfe){
+					return false;
+				}
 			}
 
 
@@ -273,11 +306,20 @@ public class BookCatalog {
 		 }
 
 		private static void logSyntaxError(String filename, String errorMsg, String record){
-			errorWriter.println("Syntax error in file: " + filename);
-			errorWriter.println("-----------------------------");
-			errorWriter.println("Error: " + errorMsg);
-			errorWriter.println("Record: " + record);
-			errorWriter.println();
+			FileWriter errorWriter;
+			try {
+				errorWriter = new FileWriter("syntax_error_file.txt");
+
+				errorWriter.write("Syntax error in file: " + filename);
+				errorWriter.write("-----------------------------");
+				errorWriter.write("Error: " + errorMsg);
+				errorWriter.write("Record: " + record);
+				errorWriter.write("");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			
 
 		} 
 	}	
